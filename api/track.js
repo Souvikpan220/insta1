317568c62491d9be6d2571c17955109010df08fc
Souvikpan220 <117498210+Souvikpan220@users.noreply.github.com>
2026-04-27 17:45:23 +0530

    Update track.js

--- api/track.js
+++ api/track.js
@@ -1,18 +1,3 @@
-const axios = require("axios");
-
-module.exports = async function(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
-  try {
-    await axios.post(process.env.VISITOR_WEBHOOK, {
-      content: "New visitor on website"
-    });
-
-    res.json({ success: true });
-
-  } catch {
-    res.json({ success: false });
-  }
+module.exports = async (req, res) => {
+  return res.json({ ok:true });
 };
